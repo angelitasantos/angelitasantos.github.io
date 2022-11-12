331cd4d3e5bb63e0a253d182a2247ba9488d7354
Angelita Santos <angelitasantos@gmail.com>
2022-11-12 19:51:00 -0300

    created header

---
+++ static/js/text.js
@@ -0,0 +1,10 @@
+/* index.html */
+
+let fullName = document.querySelector('#name');
+let selectorName = 'Angelita Santos';
+fullName.textContent = selectorName;
+
+
+let nameFunction = document.querySelector('#name-function');
+let selectorNameFunction = 'Back-end Developer';
+nameFunction.textContent = selectorNameFunction;
