95d58a5d693b02f5f6317ed59b37bd524fe7afe8
Angelita Santos <angelitasantos@gmail.com>
2022-11-13 17:11:33 -0300

    include information fullname

--- static/js/companyData.js
+++ static/js/companyData.js
@@ -1,19 +1,19 @@
 let companyJson = [
-    { id: 1, name: 'Ponto Mágico', resume: 'ago/2022 - atual', description: 'Atividades dos setores de contas a receber e faturamento.' },
+    { id: 1, name: 'Ponto Mágico', fullName: 'Ponto Mágico Comércio e Importação', resume: 'ago/2022 - atual', description: 'Atividades dos setores de contas a receber e faturamento.' },
     {
-        id: 2, name: 'Bricoflex', resume: 'mar/2021 a jan/2022', description: 'Apuração dos custos industriais e\
+        id: 2, name: 'Bricoflex', fullName: 'Bricoflex Indústria, Comércio e Importação', resume: 'mar/2021 a jan/2022', description: 'Apuração dos custos industriais e\
      análise das variações nos apontamentos de produção, perdas produtivas e suas respectivas causas\
      (comparativo mensal do custo padrão x realizado); análise de processos provendo informações \
      crı́ticas de custos ao gestor visando melhoria de produtividade.' },
     {
-        id: 3, name: 'Lucio Peças', resume: 'ago/2018 a abr/2020', description: 'Atividades de contas a\
+        id: 3, name: 'Lucio Peças', fullName: 'Lucio Auto Peças', resume: 'ago/2018 a abr/2020', description: 'Atividades de contas a\
          receber e contas a pagar. Emissão de notas fiscais e boletos de cobrança. Controle de cartões\
          (site x sistema). Cobrança de clientes em atraso. Análise de crédito e mudança de perfil de \
          cliente no sistema, caso necessário. Conferência diária de caixa e movimentação bancária. \
          Acompanhamento do orçamento previsto x realizado. Elaboração do resultado mensal (vendas x \
             compras, recebimentos x pagamentos).' },
     {
-        id: 4, name: 'Alumipack', resume: 'nov/2009 a set/2017', description: 'Coordenação de Custos nas atividades\
+        id: 4, name: 'Alumipack', fullName: 'Alumipack Indústria e Comércio', resume: 'nov/2009 a set/2017', description: 'Coordenação de Custos nas atividades\
          de planejamento e controle de recursos industriais e logı́sticos; apuração dos custos industriais e\
           análise das variações nos apontamentos de produção, perdas produtivas e suas respectivas causas\
             (comparativo mensal do custo padrão x realizado); formação do preço de venda por estado e por\
@@ -22,11 +22,11 @@ let companyJson = [
              negócio e linhas de produto (comparativo mensal previsto x realizado); elaboração de relatórios\
               gerenciais, análise mensal dos indicadores de desempenho por área e apoio na elaboração de \
               plano de ação e estratégias de gestão para cada área; ' },
-    { id: 5, name: 'GRC', resume: 'jan/2008 a out/2009', description: 'Atividades dos setores de contas a pagar, a receber e faturamento.' },
-    { id: 6, name: 'W.C.A', resume: 'jul/2005 a dez/2007', description: 'Atendimento ao cliente (venda de serviços de montagem de stand em eventos).' },
-    { id: 7, name: 'W.R.S', resume: 'jul/2003 a jun/2005', description: 'Contas a receber e cobrança.' },
-    { id: 8, name: 'Orthocrin', resume: 'set/2001 a fev/2003', description: 'Apuração de custos industriais e atualização da estrutura de custo dos produtos.' },
-    { id: 9, name: 'Comercial Marley', resume: 'dez/1999 a jul/2001', description: 'Atividades de contas a receber.' },
-    { id: 10, name: 'Profarma', resume: 'dez/1996 a mar/1999', description: 'Apuração de custos de distribuição.' },
-    { id: 11, name: 'San Belô', resume: 'mar/1996 a out/1996', description: 'Auxiliar o controle de estoque.' }
+    { id: 5, name: 'GRC', fullName: 'GRC Distribuidora', resume: 'jan/2008 a out/2009', description: 'Atividades dos setores de contas a pagar, a receber e faturamento.' },
+    { id: 6, name: 'W.C.A', fullName: 'W.C.A Locação de Equipamentos', resume: 'jul/2005 a dez/2007', description: 'Atendimento ao cliente (venda de serviços de montagem de stand em eventos).' },
+    { id: 7, name: 'W.R.S', fullName: 'W.R.S Auto Peças', resume: 'jul/2003 a jun/2005', description: 'Contas a receber e cobrança.' },
+    { id: 8, name: 'Orthocrin', fullName: 'Orthocrin Indústria e Comércio', resume: 'set/2001 a fev/2003', description: 'Apuração de custos industriais e atualização da estrutura de custo dos produtos.' },
+    { id: 9, name: 'Comercial Marley', fullName: 'Comercial Marley', resume: 'dez/1999 a jul/2001', description: 'Atividades de contas a receber.' },
+    { id: 10, name: 'Profarma', fullName: 'Profarma Dist. Produtos Farmacêuticos', resume: 'dez/1996 a mar/1999', description: 'Apuração de custos de distribuição.' },
+    { id: 11, name: 'San Belô', fullName: 'San Belô Restaurante', resume: 'mar/1996 a out/1996', description: 'Auxiliar o controle de estoque.' }
 ];
