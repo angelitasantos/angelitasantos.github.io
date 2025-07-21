7fc29c125bb8622133016846ab5613c5ab08e834
Angelita Santos <angelitasantos@gmail.com>
2025-07-21 08:30:35 -0300

    refatora header e multi-language

--- static/js/header.js
+++ static/js/header.js
@@ -1,43 +1,46 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const profileData = {
-        name: 'Angelita Santos',
-        role: 'Back-end Developer Jr',
-        linkedin: 'https://www.linkedin.com/in/angelitasantos/',
-        github: 'https://github.com/angelitasantos'
-    };
+async function loadHeader() {
+    try {
+        const response = await fetch("static/data/header.json");
+        const profileData = await response.json();
 
-    const header = document.getElementById('header');
-    header.innerHTML = `
-        <header class='header-wrapper'>
+        const header = document.getElementById("header");
+        header.innerHTML = `
+        <header class="header-wrapper">
 
-            <section class='checkbox-container'>
-                <div class='header-container'>
-                    <nav class='nav-menu'>
-                        <div class='language'>
-                            <div class='language-selected'>pt-BR</div>
+            <section class="checkbox-container">
+                <div class="header-container">
+                    <nav class="nav-menu">
+                        <div class="language">
+                            <div class="language-selected">${profileData.defaultLanguage}</div>
                             <ul>
-                                <li><a href='#' class='br'>Português</a></li>
-                                <li><a href='#' class='en'>English</a></li>
-                                <li><a href='#' class='es'>Español</a></li>
+                                <li><a href="#" class="br">Português</a></li>
+                                <li><a href="#" class="en">English</a></li>
+                                <li><a href="#" class="es">Español</a></li>
                             </ul>
                         </div>
                     </nav>
                 </div>
             </section>
 
-            <section class='personal-data'>
-                <h1 id='name'>${profileData.name}</h1>
+            <section class="personal-data">
+                <h1 id="name">${profileData.name}</h1>
                 <figure>
-                    <img id='photo' src='static/img/angelita.png' alt='${profileData.name}'>
+                    <img id="photo" src="${profileData.photo}" alt="${profileData.name}">
                 </figure>
-                <h2 id='name-function'>${profileData.role}</h2>
+                <h2 id="name-function">${profileData.role}</h2>
             </section>
 
-            <section class='social-media'>
-                <a href='${profileData.linkedin}' target='_blank'>Linkedin</a>
-                <a href='${profileData.github}' target='_blank'>Github</a>
+            <section class="social-media">
+                <a href="${profileData.linkedin}" target="_blank">Linkedin</a>
+                <a href="${profileData.github}" target="_blank">Github</a>
             </section>
-            
+
         </header>
-    `;
-});
+        `;
+
+    } catch (error) {
+        console.error("Erro ao carregar o arquivo header.json!", error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", loadHeader);
